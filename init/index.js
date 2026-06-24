adb85c16f2df9a1cf48e3136246d6135555f3c07
BalakaLaluthVardhan <laluthvardhanbalaka@gmail.com>
2026-06-24 15:08:15 +0530

    Update seeding script to use ATLASDB_URL env var

--- init/index.js
+++ init/index.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
+const dbUrl = process.env.ATLASDB_URL || 'mongodb://127.0.0.1:27017/wanderlust';
+
 main()
     .then(()=>{
         console.log("Connected to DB")
@@ -9,7 +11,7 @@ main()
     .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+  await mongoose.connect(dbUrl);
 }
 
 const initDB = async () => {
